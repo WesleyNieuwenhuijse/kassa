67bcf78e06ce1b228c3344392f7e5c819e5e395d
Ahmet Özkan <ahmedozkan_1@hotmail.com>
2018-12-05 11:40:05 +0100

    seeders gefixed :D

--- app/Product.php
+++ app/Product.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['id', 'name', 'cost_price', 'sale_price', 'subcategorie_id'];
+
+    public static function create(array $array, array $array1, array $array2)
+    {
+    }
+
     public function subcatagorie()
     {
         return $this->hasOne('App\subcatagorie');
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+//        $this->call(UsersTableSeeder::class);
+//        Model::unguard();
+//        Eloquent::unguard();
+        $this->call(ProductSeeder::class);
+//        Model::reguard();
     }
 }
